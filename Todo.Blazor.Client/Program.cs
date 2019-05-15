2d001a9dd8f1fd580ba357cba72eea75f644fed0
ken.jones <ken.jones@tightlines.io>
2019-05-14 20:46:55 -0400

    Initial commit

---
+++ Todo.Blazor.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace Todo.Blazor.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
---
+++ Todo.Blazor.Shared/SharedClient/Models/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Todo.Blazor.Shared.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public partial class TodoItem
+    {
+        [Required]
+        public long Id { get; set; }
+
+        [Required]
+        public long ListId { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        [Required]
+        [MaxLength(125)]
+        public string Title { get; set; }
+    }
+}
